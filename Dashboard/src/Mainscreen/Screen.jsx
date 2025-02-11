c9f826be2ca6a7f68332e4752e41e8f4cbb25d9e
Furkan-Reyhan <furkanreyhan585858@gmail.com>
2025-02-11 04:01:56 +0300

    screen.css ilerledi

--- Dashboard/src/Mainscreen/Screen.jsx
+++ Dashboard/src/Mainscreen/Screen.jsx
@@ -18,31 +18,43 @@ export default function Screen({ navbarHeight, slidebarWidth }) {
           className={`tab-button ${activeTab === "tab1" ? "active" : ""}`}
           onClick={() => setActiveTab("tab1")}
         >
-          Tab 1
+          <h2>Satış</h2> 
+          <p>Bugünki Satış</p>
         </button>
         <button
           className={`tab-button ${activeTab === "tab2" ? "active" : ""}`}
           onClick={() => setActiveTab("tab2")}
         >
-          Tab 2
+          <h2>Envanter</h2>
+          <p>Aktif</p>
         </button>
         <button
           className={`tab-button ${activeTab === "tab3" ? "active" : ""}`}
           onClick={() => setActiveTab("tab3")}
         >
-          Tab 3
+          <h2>Siparişler</h2>
+          <p>Son 30 Gün</p>
         </button>
         <button
           className={`tab-button ${activeTab === "tab4" ? "active" : ""}`}
           onClick={() => setActiveTab("tab4")}
         >
-          Tab 4
+          <h2>Mesajlar</h2>
+          <p>Aktif</p>
         </button>
         <button
           className={`tab-button ${activeTab === "tab5" ? "active" : ""}`}
           onClick={() => setActiveTab("tab5")}
         >
-          Tab 5
+          <h2>Tahmini Kar</h2>
+          <p>Son 30 Gün</p>
+        </button>
+        <button
+          className={`tab-button ${activeTab === "tab6" ? "active" : ""}`}
+          onClick={() => setActiveTab("tab6")}
+        >
+          <h2>Hesap Sağlığı</h2>
+          <p>Son Güncelleme:</p> 
         </button>
       </div>
 
@@ -50,27 +62,32 @@ export default function Screen({ navbarHeight, slidebarWidth }) {
       <div className="content">
         {activeTab === "tab1" && (
           <div className="tab-content">
-            <h1>Tab 1 İçeriği</h1>
+            <h1>Satış</h1>
           </div>
         )}
         {activeTab === "tab2" && (
           <div className="tab-content">
-            <h1>Tab 2 İçeriği</h1>
+            <h1>Envanter</h1>
           </div>
         )}
         {activeTab === "tab3" && (
           <div className="tab-content">
-            <h1>Tab 3 İçeriği</h1>
+            <h1>Siperişler</h1>
           </div>
         )}
         {activeTab === "tab4" && (
           <div className="tab-content">
-            <h1>Tab 4 İçeriği</h1>
+            <h1>Mesajlar</h1>
           </div>
         )}
         {activeTab === "tab5" && (
           <div className="tab-content">
-            <h1>Tab 5 İçeriği</h1>
+            <h1>Tahmini Kar</h1>
+          </div>
+        )}
+        {activeTab === "tab6" && (
+          <div className="tab-content">
+            <h1>Hesap Sağlığı</h1>
           </div>
         )}
       </div>
